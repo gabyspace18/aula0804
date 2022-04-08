4dca04f43b1625a94b7b5e0a56a7bd5a96a9da56
gabriely <biestrelinha2004@gmail.com>
2022-04-08 09:26:57 -0300

    versao 1.0.0

--- app.js
+++ app.js
@@ -3,13 +3,22 @@
 // Opção 2: importe as funções circleArea() e squareArea() usando a desestruturação de objetos aqui
 //Use os métodos importados circleArea() e squareArea() aqui
 //Imprima os valores de areaOfCircle e areaOfSquare aqui
-const converters = require('./shape-area.js');
+const { circleArea, squareArea } = require('./shape-area.js');
 
-const raio = 5;
-const lado = 10;
+const raio = process.argv[2];
+const lado = process.argv[3];
 
-const raio = converters.circleArea(raio);
+const areaOfCircle = circleArea(raio);
+const areaOfSquare = squareArea(lado);
+
+console.log(` A area do circulo é =${areaOfCircle} `);
+console.log(` A area do quadrado é =${areaOfSquare} `);
+
+
+
+
+/*const raio = converters.circleArea(raio);
 const lado = converters.squareArea(lado);
 
 console.log(`AREA OF THE CIRCLE IS ${areaOfCircle}`);
-console.log(`AREA OF THE SQUARE IS ${areaOfSquare}`);
+console.log(`AREA OF THE SQUARE IS ${areaOfSquare}`);*/
